0f7fa4b6a270775e3071316de0f40e00dc8a97ea
dglee-me <dglee.dev@gmail.com>
2024-08-29 19:52:06 +0900

    commit 3 - data fetching

--- onebite-next/section04/src/app/(with-searchbar)/page.tsx
+++ onebite-next/section04/src/app/(with-searchbar)/page.tsx
@@ -1,6 +1,5 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
 import { BookData } from "@/types";
 
 async function AllBooks() {
@@ -21,7 +20,8 @@ async function AllBooks() {
 
 async function RecoBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다 ...</div>;
